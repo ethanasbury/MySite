05471a6a8c8f055359df7c5072a44321516b55f0
Ethan Asbury <ethanasbury@gmail.com>
2015-06-09 16:05:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	$("li#home").mouseenter(function(){
 		$("li#home").fadeTo('slow', 0.50)
-		/*$("li#home").effect( "bounce", {times:1}, 500 );*/
+		$("li#home").effect( "bounce", {direction: 'up', distance: 7, times: 1}, 500 );
 	});
 	$("li#home").mouseleave(function(){
 		$("li#home").fadeTo('fast', 1.00)
@@ -10,6 +10,7 @@ $(document).ready(function(){
 	
 	$("li#about").mouseenter(function(){
 		$("li#about").fadeTo('slow', 0.50)
+		$("li#about").effect( "bounce", {direction: 'up', distance: 7, times: 1}, 500 );
 	});
 	$("li#about").mouseleave(function(){
 		$("li#about").fadeTo('fast', 1.00)
@@ -17,6 +18,7 @@ $(document).ready(function(){
 
 	$("li#examples").mouseenter(function(){
 		$("li#examples").fadeTo('slow', 0.50)
+		$("li#examples").effect( "bounce", {direction: 'up', distance: 7, times: 1}, 500 );
 	});
 	$("li#examples").mouseleave(function(){
 		$("li#examples").fadeTo('fast', 1.00)
