0c8a22baf6c2ff1a2777a8f936e1b8e57a5ab95f
Ethan Asbury <ethanasbury@gmail.com>
2015-06-09 15:28:54 -0700

    Added fade functions

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 /*New JQuery file 06-09-2015*/
-$(document).ready({});
+$(document).ready(function(){
+	$("li#home").mouseenter(function(){
+		$("li#home").fadeTo('slow', 0.50)
+	});
+	$("li#home").mouseleave(function(){
+		$("li#home").fadeTo('fast', 1.00)
+	});
+	
+	$("li#about").mouseenter(function(){
+		$("li#about").fadeTo('slow', 0.50)
+	});
+	$("li#about").mouseleave(function(){
+		$("li#about").fadeTo('fast', 1.00)
+	});
+
+	$("li#examples").mouseenter(function(){
+		$("li#examples").fadeTo('slow', 0.50)
+	});
+	$("li#examples").mouseleave(function(){
+		$("li#examples").fadeTo('fast', 1.00)
+	});
+});
