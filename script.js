9292da627274f36df43f3149138525ea8f9f60b4
Ethan Asbury <ethanasbury@gmail.com>
2015-06-17 16:15:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
 /*New JQuery file 06-09-2015*/
+
 $(document).ready(function(){
-	$("li#home").mouseenter(function(){
-		$("li#home").fadeTo('slow', 0.50)
-		$("li#home").effect( "bounce", {direction: 'up', distance: 7, times: 1}, 500 );
-	});
-	$("li#home").mouseleave(function(){
-		$("li#home").fadeTo('fast', 1.00)
-	});
 	
-	$("li#about").mouseenter(function(){
-		$("li#about").fadeTo('slow', 0.50)
-		$("li#about").effect( "bounce", {direction: 'up', distance: 7, times: 1}, 500 );
-	});
-	$("li#about").mouseleave(function(){
-		$("li#about").fadeTo('fast', 1.00)
+	$("li#home").mouseenter(function(){
+
+		$("li#home").fadeTo('slow', 0.50)
+
+		$("li#home").effect( "bounce", {direction: 'up', distance: 3, times: 1}, 300 );
+
 	});
 
+	
+	$("li#home").mouseleave(function(){
+
+		$("li#home").fadeTo('fast', 1.00)
+
+	});
+
+
+	$("li#about").mouseenter(function(){
+
+		$("li#about").fadeTo('slow', 0.50)
+
+		$("li#about").effect( "bounce", {direction: 'up', distance: 3, times: 1}, 300 );
+
+	});
+	
+
+	$("li#about").mouseleave(function(){
+
+		$("li#about").fadeTo('fast', 1.00)
+	
+	});
+
+	
+
 	$("li#examples").mouseenter(function(){
+
 		$("li#examples").fadeTo('slow', 0.50)
-		$("li#examples").effect( "bounce", {direction: 'up', distance: 7, times: 1}, 500 );
+
+		$("li#examples").effect( "bounce", {direction: 'up', distance: 3, times: 1}, 300 );
+
 	});
+	
+
 	$("li#examples").mouseleave(function(){
+
 		$("li#examples").fadeTo('fast', 1.00)
+
 	});
+
+
+
+    	$("#button").click(function() {
+        	var toAdd = $("input[name=checkListItem]").val();
+        	$(".list").append('<div class="articles">' + toAdd + '</div>');
+    	});
+    	$(document).on('click','.item', function(){
+        	$(this).remove();
+    	});
 });
+
